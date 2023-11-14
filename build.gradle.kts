b66e105924e70aead76b28127e861f0c633d5aea
jhshin <jhshin@dozn.co.kr>
2023-11-14 09:10:20 +0900

    add actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,10 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
