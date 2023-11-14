04d0712ab0861bef15f4052f764168e2c0a85261
jhshin <jhshin@dozn.co.kr>
2023-11-14 17:02:48 +0900

    add logger & logback-spring.xml

--- src/main/kotlin/com/jaehong/grafanademo/controller/HelloController.kt
+++ src/main/kotlin/com/jaehong/grafanademo/controller/HelloController.kt
@@ -1,5 +1,6 @@
 package com.jaehong.grafanademo.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,6 +9,11 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/demo/v1")
 class HelloController {
 
+  private val logger = LoggerFactory.getLogger(HelloController::class.java)
+
   @GetMapping("/hello")
-  fun helloWorld(): String = "hello world"
+  fun helloWorld(): String {
+    logger.info("hello world")
+    return "hello world"
+  }
 }
