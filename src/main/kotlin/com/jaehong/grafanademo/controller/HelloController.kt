4d3277a5ce136291b17edaa7e735edf69f334c32
yrnam <yrnam@dozn.co.kr>
2023-11-29 11:32:08 +0900

    controller 수정

--- src/main/kotlin/com/jaehong/grafanademo/controller/HelloController.kt
+++ src/main/kotlin/com/jaehong/grafanademo/controller/HelloController.kt
@@ -1,7 +1,9 @@
 package com.jaehong.grafanademo.controller
 
 import com.jaehong.grafanademo.service.HelloService
+import com.jaehong.grafanademo.response.HelloResponse
 import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -17,5 +19,11 @@ class HelloController(private val service: HelloService) {
     val str = service.sayHello()
     logger.info(">> Service said $str")
     return str
+  }
+  @GetMapping("helloLoki")
+  fun helloWorldLoki(): ResponseEntity<HelloResponse> {
+    val response = HelloResponse(1,"hello World")
+    logger.info("$response")
+    return ResponseEntity.ok().body(response)
   }
 }
---
+++ src/main/kotlin/com/jaehong/grafanademo/response/HelloResponse.kt
@@ -0,0 +1,7 @@
+package com.jaehong.grafanademo.response
+
+
+data class HelloResponse(
+  val id: Int,
+  val message: String
+)
