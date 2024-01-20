b752604914e35a10e46aafce8785ab0f3977bae0
woghd9072 <woghd9072@gmail.com>
2024-01-20 13:18:21 +0900

    refactor controller

--- src/main/kotlin/com/jaehong/grafanademo/controller/HelloController.kt
+++ src/main/kotlin/com/jaehong/grafanademo/controller/HelloController.kt
@@ -1,10 +1,7 @@
 package com.jaehong.grafanademo.controller
 
 import com.jaehong.grafanademo.service.HelloService
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.jaehong.grafanademo.response.HelloResponse
 import org.slf4j.LoggerFactory
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -14,27 +11,11 @@ import org.springframework.web.bind.annotation.RestController
 class HelloController(private val service: HelloService) {
 
   private val logger = LoggerFactory.getLogger(HelloController::class.java)
-  private val mapper = ObjectMapper()
 
   @GetMapping("/hello")
   fun helloWorld(): String {
     val str = service.sayHello()
     logger.info(">> Service said $str")
     return str
-  }
-  @GetMapping("helloLoki")
-  fun helloWorldLoki(): ResponseEntity<HelloResponse> {
-    val random = generateRandom()
-    val response = HelloResponse(id = random, message = "HelloWorld")
-    val value = mapper.writeValueAsString(response)
-    logger.info(value)
-
-    service.convert(response)
-    return ResponseEntity.ok().body(response)
-  }
-
-  fun generateRandom(): String{
-    val random = (1..1000).random()
-    return "003I0000$random"
   }
 }
